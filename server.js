7dc1663dbb9eb5dd94c26a4b612de16e2770953a
Adrian <1801050@sit.singaporetech.edu.sg>
2020-05-27 16:16:09 +0800

    Update server.js

--- server.js
+++ server.js
@@ -100,9 +100,7 @@ if(process.env.NODE_ENV === 'production'){
     //set static folder
     app.use(express.static('build'));
 }
-app.get('*',(req, res) => {
-    res.sendFile(path.resolve(__dirname, 'build', 'index.html'));
-});
+
 
 const port = process.env.PORT || 4000;
 app.use('/api', apiRoute);
